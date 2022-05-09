b749486c7a1a0162ceab4a422f37990f5844652b
Yousef M. Eldaly <94533646+YousefEldaly@users.noreply.github.com>
2022-05-09 13:09:41 +0200

    Add files via upload

---
+++ js/Greece.js
@@ -0,0 +1,22 @@
+alert("Welcome, To Greece Site");
+function hello() {
+    let txt;
+    let name = prompt("Please Enter Your Name :","");
+    if(name==null || name==""){
+        txt="User Cancelled.";
+    }
+    else{
+        txt="Hello" + name + "! How Are You ?";
+    }
+    document.getElementById("hell").innerHTML=txt;
+}
+hello();
+
+function f1(){
+
+    document.getElementById('b1').style.display='block';
+       
+}
+function f2(){
+    document.getElementById('b1').style.display='none';
+}
